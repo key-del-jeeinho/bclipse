7859c631b433f7bb250a1c89d18b465995c11cf2
raul <raul@ohoolabs.com>
2024-02-04 02:21:12 +0900

    application/fix: Base64UUID
    (cherry picked from commit 99420713a50cb0ab8e245aed556141f9008d71cc)

--- bclipse-application/src/main/kotlin/com/bclipse/application/common/domain/Base64UUID.kt
+++ bclipse-application/src/main/kotlin/com/bclipse/application/common/domain/Base64UUID.kt
@@ -1,8 +1,8 @@
 package com.bclipse.application.common.domain
 
+import com.bclipse.application.infra.kotlin.LazyPrecondition.require
 import java.nio.ByteBuffer
-import java.util.Base64
-import java.util.UUID
+import java.util.*
 
 @JvmInline
 value class Base64UUID private constructor(val value: String) {
@@ -11,17 +11,37 @@ value class Base64UUID private constructor(val value: String) {
 
         fun generate(): Base64UUID = from(UUID.randomUUID())
 
-        fun fromEncodedString(encodedString: String): Base64UUID {
-            val prefixRemoved = encodedString.removePrefix(PREFIX)
-            require(
-                encodedString.startsWith(PREFIX)
-                    && isBase64UUIDFormat(prefixRemoved)
-            ) { "올바르지 않은 포맷입니다! - '$encodedString'" }
+        fun fromEncodedString(base64UUID: String): Base64UUID {
+            val lazyMessage = lazy { "올바르지 않은 포맷입니다! - '$base64UUID'" }
 
-            return Base64UUID(encodedString)
+            require(base64UUID.startsWith(PREFIX), lazyMessage)
+            val encodedString = base64UUID.removePrefix(PREFIX)
+            requireBase64UUIDFormat(encodedString, lazyMessage)
+
+            return Base64UUID(base64UUID)
         }
 
-        private fun from(uuid: UUID): Base64UUID {
+        private fun requireBase64UUIDFormat(encodedString: String, lazyMessage: Lazy<String>) {
+            val decoder = Base64.getUrlDecoder()
+            try {
+                val uuidBytes = decoder.decode(encodedString)
+                val restoredUUID = ByteBuffer.wrap(uuidBytes).let {
+                    val uuid = UUID(it.long, it.long)
+                    require(!it.hasRemaining()) { "decoded byte is not UUID format" }
+                    uuid
+                }
+                val reEncodedString = encode(restoredUUID)
+                require(reEncodedString == encodedString) {
+                    "re encoded string is not same - '$encodedString' '${reEncodedString}'"
+                }
+            } catch(throwable: Throwable) {
+                throw IllegalArgumentException(lazyMessage.value, throwable)
+            }
+        }
+
+        private fun from(uuid: UUID): Base64UUID = uuid.let(::encode).let(::addPrefix)
+
+        private fun encode(uuid: UUID): String {
             val uuidBytes = ByteBuffer.wrap(ByteArray(16)).apply {
                 putLong(uuid.mostSignificantBits)
                 putLong(uuid.leastSignificantBits)
@@ -31,20 +51,9 @@ value class Base64UUID private constructor(val value: String) {
                 .withoutPadding()
                 .encodeToString(uuidBytes)
 
-            return Base64UUID(PREFIX + base64UrlEncodedUUID)
+            return base64UrlEncodedUUID
         }
 
-        private fun isBase64UUIDFormat(encodedString: String): Boolean =
-            Base64.getUrlDecoder().runCatching {
-                val uuidBytes = decode(encodedString)
-                val restoredUUID = ByteBuffer.wrap(uuidBytes).let {
-                    val uuid = UUID(it.long, it.long)
-                    require(!it.hasRemaining()) { "decoded byte is not UUID format" }
-                    uuid
-                }
-                val reEncodedString = from(restoredUUID)
-
-                reEncodedString.value == encodedString
-            }.getOrElse { false }
+        private fun addPrefix(encodedString: String) =  Base64UUID(PREFIX + encodedString)
     }
 }
